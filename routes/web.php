e9bcbd1b5db4054d427332d801ca11c6dfe16958
choccoo4 <aulia.anggraiini009@gmail.com>
2025-04-27 21:02:22 +0700

    Update susunaan folder

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,61 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/home', function () {
-    return view('pages.home');
+    return view('buyer.home');
 })->name('home');
 
 Route::get('/shop', function () {
-    return view('pages.shop');
+    return view('buyer.shop');
 })->name('shop');
 
 Route::get('/best-sellers', function () {
-    return view('pages.best-sellers');
+    return view('buyer.best-sellers');
 })->name('best-sellers');
 
 Route::get('/gifts', function () {
-    return view('pages.gifts');
+    return view('buyer.gifts');
 })->name('gifts');
 
 Route::get('/product-detail', function () {
-    return view('pages.product-detail');
+    return view('buyer.product-detail');
 })->name('product-detail');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/daftarproduk', function () {
+    return view('daftarproduk');
+})->name('produk.index');
+
+Route::get('/tambahproduk', function () {
+    return view('tambahproduk');
+})->name('tambahproduk');
+
+Route::get('/updateproduk', function () {
+    return view('updateproduk');
+})->name('updateproduk');
+
+Route::get('/daftarpesanan', function () {
+    return view('daftarpesanan');
+})->name('pesanan.index');
+
+Route::get('/rekapitulasi', function () {
+    return view('rekapitulasi');
+})->name('rekap.index');
+
+
+Route::get('/laporan', function () {
+    return view('laporan');
+});
+
+Route::get('/pengaturan', function () {
+    return view('pengaturan');
+})->name('pengaturan.index');
+
+//Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+
